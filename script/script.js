703bfe3a7a59734f37edafdddf8f67b23a5b06d2
Ilya Plank <ilplank@yandex.ru>
2019-09-02 22:12:09 +0500

    test-b

--- script/script.js
+++ script/script.js
@@ -42,7 +42,7 @@ if ('serviceWorker' in navigator) {
 const menu = document.querySelector('.menu');
 const btn = menu.querySelector('.nav-tgl');
 const option = document.querySelector('.option');
-btn.addEventListener('click', evt => {
+btn.addEventListener('click', event => {
 	if (menu.className.indexOf('active') === -1) {
 		menu.classList.add('active');
 		option.classList.remove('d-n');
@@ -56,12 +56,65 @@ btn.addEventListener('click', evt => {
 
 /* firstScreen */
 
+const video = document.querySelector('.main-video');
+
 const firstScreen = document.querySelector('.first-screen');
-const youtube = document.querySelector('.youtube');
+const videoScreen = document.querySelector('.video-screen');
 const startBtn = document.querySelector('.start-btn');
-startBtn.addEventListener('click', evt => {
+startBtn.addEventListener('click', event => {
 	firstScreen.classList.toggle('d-n');
-	youtube.classList.toggle('d-n');
+	videoScreen.classList.toggle('d-n');
+	playVideo();
 });
 
 /* firstScreen END*/
+
+
+
+
+
+/* video */
+
+const playBtn = document.querySelector('.play-btn');
+const topLeft = document.querySelector('.top-left');
+const topRight = document.querySelector('.top-right');
+const bottomLeft = document.querySelector('.bottom-left');
+const bottomRight = document.querySelector('.bottom-right');
+const center = document.querySelector('.center');
+
+topLeft.addEventListener('click', event => {
+	video.src = 'video/01.mp4';
+	playVideo();
+});
+
+topRight.addEventListener('click', event => {
+	video.src = 'video/02.mp4';
+	playVideo();
+});
+
+bottomLeft.addEventListener('click', event => {
+	video.src = 'video/03.mp4';
+	playVideo();
+});
+
+bottomRight.addEventListener('click', event => {
+	video.src = 'video/04.mp4';
+	playVideo();
+});
+
+center.addEventListener('click', event => {
+	video.src = 'video/05.mp4';
+	playVideo();
+});
+
+function playVideo() {
+	playBtn.classList.add('d-n');
+	video.play();
+	video.addEventListener('ended', endedVideo);
+}
+
+function endedVideo() {
+	playBtn.classList.remove('d-n');
+}
+
+/* video END*/
